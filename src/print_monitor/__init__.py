79fb834c28812104942f87b2971a05288c38fa2c
enzo-going <170044990+enzo-going@users.noreply.github.com>
2026-07-30 11:16:42 -0300

    chore: prepara versao 1.2.0

--- src/print_monitor/__init__.py
+++ src/print_monitor/__init__.py
@@ -4,4 +4,4 @@ O pacote coleta contadores totais de impressao, persiste o historico em SQLite
 e calcula o volume por periodo pela diferenca entre leituras.
 """
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
